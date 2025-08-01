e89ce76cd6f4a63a5d3d0a5915d307b314c2164a
devdragon7 <ray993500@gmail.com>
2025-08-01 03:46:59 -0700

    commit & push

--- src/ai-editors/scripts/ai-edit.ts
+++ src/ai-editors/scripts/ai-edit.ts
@@ -1,5 +1,29 @@
 #!/usr/bin/env node
-
+```yaml annotate
+# The name of the workflow as it will appear in the "Actions" tab of the GitHub repository.
+name: Post welcome comment
+# The `on` keyword lets you define the events that trigger when the workflow is run.
+on:
+  # Add the `pull_request` event, so that the workflow runs automatically
+  # every time a pull request is created.
+  pull_request:
+    types: [opened]
+# Modifies the default permissions granted to `GITHUB_TOKEN`.
+permissions:
+  pull-requests: write
+# Defines a job with the ID `build` that is stored within the `jobs` key.
+jobs:
+  build:
+    name: Post welcome comment
+    # Configures the operating system the job runs on.
+    runs-on: ubuntu-latest
+    # The `run` keyword tells the job to execute the [`gh pr comment`](https://cli.github.com/manual/gh_pr_comment) command on the runner.
+    steps:
+      - run: gh pr comment $PR_URL --body "Welcome to the repository!"
+        env:
+          GH_TOKEN: $
+          PR_URL: $
+```
 import { fileURLToPath } from 'url'
 import { Command } from 'commander'
 import fs from 'fs'
